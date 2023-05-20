f52320a10d4e3fc5e8be34d80ce1269b3462fe57
Sam Rahnama <meysam.rah.97@gmail.com>
2023-05-20 19:29:52 +0330

    model bining package added.

--- app/Controllers/Http/UsersController.ts
+++ app/Controllers/Http/UsersController.ts
@@ -1,5 +1,9 @@
 import type {HttpContextContract} from '@ioc:Adonis/Core/HttpContext'
 import Database from "@ioc:Adonis/Lucid/Database";
+import User from 'App/Models/User';
+import RegisterUserValidator from 'App/Validators/RegisterUserValidator';
+import UpdateUserValidator from 'App/Validators/UpdateUserValidator';
+import { bind } from '@adonisjs/route-model-binding'
 
 export default class UsersController {
   public async index(ctx: HttpContextContract) {
@@ -7,15 +11,28 @@ export default class UsersController {
     const users = await Database.from('users').paginate(page, 8)
     return users.toJSON()
   }
-  public async store() {
+
+  public async store({request}: HttpContextContract) {
+    const payload = await request.validate(RegisterUserValidator)
+    const user = await User.create(payload)
+    return user
   }
 
-  public async show() {
+  @bind()
+  public async show({}, user: User) {
+    return user
   }
 
-  public async update() {
+  @bind()
+  public async update({request}: HttpContextContract, user: User) {
+    const payload = await request.validate(UpdateUserValidator)
+    await user.merge(payload).save()
+    return user
   }
 
-  public async destroy() {
+  @bind()
+  public async destroy({}, user: User) {
+    await user.delete()
+    return user
   }
 }
