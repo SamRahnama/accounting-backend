8fd386eb82d3977f4a8643ad4e6164fc2b4065ee
Sam Rahnama <meysam.rah.97@gmail.com>
2023-05-20 18:31:27 +0330

    discount type added.

--- app/Validators/UpdatePaymentValidator.ts
+++ app/Validators/UpdatePaymentValidator.ts
@@ -1,14 +1,17 @@
-import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
-import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-import { PaymentTypes } from 'App/Enums/payments.enum'
+import {schema, CustomMessages, rules} from '@ioc:Adonis/Core/Validator'
+import type {HttpContextContract} from '@ioc:Adonis/Core/HttpContext'
+import {PaymentTypes} from 'App/Enums/payments.enum'
+import {DiscountTypes} from 'App/Enums/discounts'
 
 export default class UpdatePaymentValidator {
-  constructor(protected ctx: HttpContextContract) {}
+  constructor(protected ctx: HttpContextContract) {
+  }
 
   public schema = schema.create({
     type: schema.enum.optional(Object.values(PaymentTypes)),
     paid_amount: schema.number.optional(),
     discount: schema.number.optional([rules.unsigned()]),
+    discount_type: schema.enum.optional(Object.values(DiscountTypes), [rules.requiredIfExists('discount')]),
     total: schema.number.optional([rules.unsigned()])
   })
 
