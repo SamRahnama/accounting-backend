c2b902ac83207394cd9357a4e73cfc4e24e596dc
Sam Rahnama <meysam.rah.97@gmail.com>
2023-05-20 18:29:53 +0330

    RolesController.ts created.

--- app/Controllers/Http/RolesController.ts
+++ app/Controllers/Http/RolesController.ts
@@ -1,25 +1,39 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type {HttpContextContract} from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
+import Role from 'App/Models/Role'
+import CreateRoleValidator from 'App/Validators/CreateRoleValidator'
+import UpdateRoleValidator from 'App/Validators/UpdateRoleValidator'
 
 export default class RolesController {
-  public async index() {
+  public async index({request}: HttpContextContract) {
+    let page = request.qs().page
+    let roles = await Database.from('roles').paginate(page, 8)
+    return roles.toJSON()
   }
 
-  public async create() {
-
+  public async store({request}: HttpContextContract) {
+    const payload = await request.validate(CreateRoleValidator)
+    const role = await Role.create(payload)
+    return role
   }
 
-  public async store() {
-
+  public async show({params}: HttpContextContract) {
+    const role = await Role.findOrFail(params.id)
+    return role
   }
 
-  public async show() {
+  public async update({params, request}: HttpContextContract) {
+    const role = await Role.findOrFail(params.id)
+    const payload = await request.validate(UpdateRoleValidator)
+    await role.merge(payload).save()
+    return role
   }
 
-  public async edit() {
-  }
 
-  public async update() {
-  }
-  public async destroy() {
+  public async destroy({params}: HttpContextContract) {
+    const role = await Role.findOrFail(params.id)
+    await role.delete()
+    return role
   }
 }
+
