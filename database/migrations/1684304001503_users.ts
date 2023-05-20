6620b4b06d6f2449bac54dda11848bc3f8a6dc41
Sam Rahnama <meysam.rah.97@gmail.com>
2023-05-20 18:21:37 +0330

    phone number, password, remember_me_token and role id added to users table.

--- database/migrations/1684304001503_users.ts
+++ database/migrations/1684304001503_users.ts
@@ -3,19 +3,21 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class extends BaseSchema {
   protected tableName = 'users'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
       table.string('first_name').notNullable()
       table.string('last_name').notNullable()
-      table.string('email').notNullable()
-      table.string('phone_number').notNullable()
-
-      table.timestamps(true,true)
+      table.string('phone_number').notNullable().unique()
+      table.string('email', 255).notNullable().unique()
+      table.string('password', 180).notNullable()
+      table.string('remember_me_token').nullable()
+      table.integer('role_id').unsigned().references('roles.id').onDelete('RESTRICT')
+      table.timestamps(true, true)
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
