eb5f27c17b239049d17b1d48c9b2967f89a73415
Sam Rahnama <meysam.rah.97@gmail.com>
2023-05-20 18:28:31 +0330

    LoansController created.

--- app/Controllers/Http/LoansController.ts
+++ app/Controllers/Http/LoansController.ts
@@ -1,19 +1,40 @@
-// import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import type {HttpContextContract} from '@ioc:Adonis/Core/HttpContext'
+import Database from '@ioc:Adonis/Lucid/Database'
+import Loan from 'App/Models/Loan'
+import CreateLoanValidator from 'App/Validators/CreateLoanValidator'
+import UpdateLoanValidator from 'App/Validators/UpdateLoanValidator'
 
 export default class LoansController {
-  public async index(){
-
+  public async index({request}: HttpContextContract) {
+    let page = request.qs().page
+    let payments = await Database.from('payments').paginate(page, 8)
+    return payments
   }
-  public async show(){
 
+
+  public async show({params}: HttpContextContract) {
+    const loan = await Loan.findOrFail(params.id)
+    await loan.load('payment')
+    return loan
   }
-  public async store(){
 
+  public async store({request}: HttpContextContract) {
+    const payload = await request.validate(CreateLoanValidator)
+    const loan = await Loan.create(payload)
+    await loan.load('payment')
+    return loan
   }
-  public async update(){
 
+  public async update({params, request}: HttpContextContract) {
+    const loan = await Loan.findOrFail(params.id)
+    const payload = await request.validate(UpdateLoanValidator)
+    await loan.merge(payload).save()
+    return loan
   }
-  public async destroy(){
 
+  public async destroy({params}: HttpContextContract) {
+    const loan = await Loan.findOrFail(params.id)
+    await loan.delete()
+    return loan
   }
 }
