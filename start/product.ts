57140518fb3f3f07dafe6a17977a991960d16565
Sam Rahnama <meysam.rah.97@gmail.com>
2023-05-21 16:50:40 +0330

    update product route created.

--- start/product.ts
+++ start/product.ts
@@ -1,6 +1,7 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 export default function productRoutes() {
+  Route.resource('products', "ProductsController").apiOnly().except(['store','update'])
   Route.post('products/storage/:store', "ProductsController.storeOnStorage").as('product.store')
-  Route.resource('products', "ProductsController").apiOnly().except(['store'])
+  Route.put('products/:product/storage/:store', "ProductsController.update").as('product.update')
 }
